74a824d0dc3857c46a558161094d464a6708ade0
p6nj <breee@duck.com>
2024-02-27 20:43:23 +0100

    better function, diagonals

--- square all dots.py
+++ square all dots.py
@@ -2,12 +2,14 @@ p = lambda x: print(x, end=" ")
 
 
 def line(coords: tuple[tuple[int]]):
-    return f"M {coords[0][0]} {coords[0][1]} L {coords[1][0]} {coords[1][1]}"
+    p(f"M {coords[0][0]} {coords[0][1]} L {coords[1][0]} {coords[1][1]}")
 
 
 n = 5
+line(((0, 0), (n, n)))
+line(((n, 0), (0, n)))
 for i in range(1, n):
-    p(line(((i, 0), (n - i, n))))
-    p(line(((0, i), (n, n - i))))
+    line(((i, 0), (n - i, n)))
+    line(((0, i), (n, n - i)))
 
 print()
