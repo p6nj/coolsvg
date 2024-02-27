36044d53c76b2b70ce5a84da5696ab218025311f
p6nj <breee@duck.com>
2024-02-27 23:04:36 +0100

    some pictures

--- square all dots.py
+++ square all dots.py
@@ -5,7 +5,7 @@ def line(coords: tuple[tuple[int]]):
     p(f"M {coords[0][0]} {coords[0][1]} L {coords[1][0]} {coords[1][1]}")
 
 
-n = 5
+n = 100
 line(((0, 0), (n, n)))
 line(((n, 0), (0, n)))
 for i in range(1, n):
